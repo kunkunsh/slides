b2add56e079001e6f0f712a14d65a73c6df7e622
Huakun Shen <huakun.shen@huakunshen.com>
2024-12-27 21:55:15 -0500

    refactor: update base paths in package.json and clean up config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,24 +4,8 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Slidev Monorepo",
   description: "Slidev Monorepo Template",
-  base: "/slidev-monorepo/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
-    // nav: [
-    //   { text: 'Home', link: '/' },
-    //   { text: 'Examples', link: '/markdown-examples' }
-    // ],
-
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
-
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
     ]
